7dd21e9381509d28f87731ebd102d23b487588d8
SokoMix <dimonsheu@mail.ru>
2023-10-31 19:54:53 +0300

    Uploaded requrements.txt

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from routes.routing import routes_bp
 app = Flask(__name__)
 
 app.register_blueprint(routes_bp)
-
-
-if __name__ == "__main__":
-    app.run(port=8000, debug=True)
+#
+#
+# if __name__ == "__main__":
+#     app.run(port=8000)
